69a37033ad45cfddbf936ae35e638b078432adc9
Serge Rodovnichenko <sergerod@gmail.com>
2014-05-01 14:35:40 +0400

    Модель

---
+++ lib/models/shopSyrredirPlugin.model.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Модель
+ *
+ * @author Serge Rodovnichenko <sergerod@gmail.com>
+ */
+class shopSyrredirPluginModel extends waModel
+{
+    /**
+     * Table name
+     * @var string
+     */
+    protected $table = 'shop_syrredir';
+}
