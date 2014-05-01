eeacbab8d32b57be0a3ea4cc8bd22409469c4d76
Serge Rodovnichenko <sergerod@gmail.com>
2014-05-01 14:36:13 +0400

    Описание плагина

---
+++ lib/config/plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'name' => 'Custom shop redirects',
+    'description' => 'Ручное указание редиректов',
+    'vendor'=>670917,
+    'version'=>'1.0.0',
+    'img'=>'img/redirect.png',
+    'icons'=>array(
+        16 => 'img/redirect.png',
+    ),
+    'handlers' => array(
+        'frontend_error' => 'frontendError',
+    ),
+);
